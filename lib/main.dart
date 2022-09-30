68506ac9c60d3cf5cc8cb1b9f6e7ef8c7c462e60
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2022-09-30 08:44:20 -0300

    Create Strings file / Remove widget_test / Update name project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scaffold_class_app/strings.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: Strings.APP_NAME,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
---
+++ lib/strings.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: constant_identifier_names
+
+abstract class Strings {
+  //#region General
+  static const String APP_NAME = "Gulosos App";
+  //#endregion
+
+  //#region Pages
+
+  //#region Home Page
+  static const String HOME_TITLE = "Gulosos";
+
+  static const String HOME_LABEL_PRODUCTS = "Produtos";
+
+  static const String HOME_DRAWER_HOME = "Início";
+  static const String HOME_DRAWER_CATEGORIES = "Categorias";
+  static const String HOME_DRAWER_INVITE = "Convide";
+  static const String HOME_DRAWER_ABOUT = "Sobre o app";
+
+  static const String HOME_DRAWER_LABEL_WELCOME = "Seja Bem Vindo(a)";
+
+  static const String HOME_DRAWER_BUTTON_LOGIN = "Entrar";
+  //#endregion
+
+  //#region Categories Page
+  static const String CATEGORIES_TITLE = "Categorias";
+  //#region
+
+  //#region Invite Page
+  static const String INVITE_TITLE = "Convide";
+  static const String INVITE_DESCRIPTION =
+      "Tem gostado da experiência? Avise aos seus amigos para ter essa experiência fantástica!";
+  static const String INVITE_BUTTON = "Convidar";
+  //#endregion
+
+  //#region About Page
+  static const String ABOUT_TITLE = "Sobre o App";
+  static const String ABOUT_DESCRIPTION =
+      "Um delivery prático e rápido como você nunca viu!\n\nGulosos App é onde você encontra uma variedade incrível de ofertas de produtos que são super deliciosos, explore essa incrível experiência!";
+  static const String ABOUT_VERSION = "0.1.0";
+  //#endregion
+  //#endregion
+
+  //#region Components
+  static const String COMPONENT_CATEGORY_BUTTON_VISIT = "Visitar";
+  //#endregion
+}
