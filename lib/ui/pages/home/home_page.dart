d410bce686b3a4dc565032a6508b640e34a93c8e
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2022-09-30 09:07:25 -0300

    Implement drawer

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -16,6 +16,80 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text(Strings.HOME_TITLE),
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              color: Theme.of(context).primaryColor,
+              height: 240,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Expanded(child: SizedBox()),
+                  const CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 24,
+                    child: Icon(
+                      Icons.face,
+                      size: 48,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    Strings.HOME_DRAWER_LABEL_WELCOME,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall
+                        ?.copyWith(color: Colors.white),
+                  ),
+                  const Expanded(child: SizedBox()),
+                  SizedBox(
+                    height: 48,
+                    width: double.maxFinite,
+                    child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          side: const BorderSide(color: Colors.white, width: 2),
+                        ),
+                        onPressed: () {},
+                        child: const Text(Strings.HOME_DRAWER_BUTTON_LOGIN)),
+                  )
+                ],
+              ),
+            ),
+            const Divider(
+              height: 2,
+              color: Colors.blueGrey,
+            ),
+            ListTile(
+              selected: true,
+              leading: const Icon(Icons.home),
+              title: const Text(Strings.HOME_DRAWER_HOME),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.list),
+              title: const Text(Strings.HOME_DRAWER_CATEGORIES),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.people_alt),
+              title: const Text(Strings.HOME_DRAWER_INVITE),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text(Strings.HOME_DRAWER_ABOUT),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
       body: const HomeContentPage(),
     );
   }
